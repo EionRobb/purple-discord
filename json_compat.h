753bd3117f1edf4b46de992c6ca7a31eceaf012c
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-08-13 15:01:32 -0700

    Move unrelated headers into their own files

---
+++ json_compat.h
@@ -0,0 +1,65 @@
+/*
+ *   Discord plugin for libpurple
+ *   Copyright (C) 2016-2017  Eion Robb
+ *   Copyright (C) 2017 Alyssa Rosenzweig
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <json-glib/json-glib.h>
+
+// Supress overzealous json-glib 'critical errors'
+#define json_object_has_member(JSON_OBJECT, MEMBER) \
+	(JSON_OBJECT ? json_object_has_member(JSON_OBJECT, MEMBER) : FALSE)
+#define json_object_get_int_member(JSON_OBJECT, MEMBER) \
+	(json_object_has_member(JSON_OBJECT, MEMBER) ? json_object_get_int_member(JSON_OBJECT, MEMBER) : 0)
+#define json_object_get_string_member(JSON_OBJECT, MEMBER) \
+	(json_object_has_member(JSON_OBJECT, MEMBER) ? json_object_get_string_member(JSON_OBJECT, MEMBER) : NULL)
+#define json_object_get_array_member(JSON_OBJECT, MEMBER) \
+	(json_object_has_member(JSON_OBJECT, MEMBER) ? json_object_get_array_member(JSON_OBJECT, MEMBER) : NULL)
+#define json_object_get_object_member(JSON_OBJECT, MEMBER) \
+	(json_object_has_member(JSON_OBJECT, MEMBER) ? json_object_get_object_member(JSON_OBJECT, MEMBER) : NULL)
+#define json_object_get_boolean_member(JSON_OBJECT, MEMBER) \
+	(json_object_has_member(JSON_OBJECT, MEMBER) ? json_object_get_boolean_member(JSON_OBJECT, MEMBER) : FALSE)
+
+#define json_array_get_length(JSON_ARRAY) \
+	(JSON_ARRAY ? json_array_get_length(JSON_ARRAY) : 0)
+
+static gchar *
+json_object_to_string(JsonObject *obj)
+{
+	JsonNode *node;
+	gchar *str;
+	JsonGenerator *generator;
+
+	node = json_node_new(JSON_NODE_OBJECT);
+	json_node_set_object(node, obj);
+
+	// a json string ...
+	generator = json_generator_new();
+	json_generator_set_root(generator, node);
+	str = json_generator_to_data(generator, NULL);
+	g_object_unref(generator);
+	json_node_free(node);
+
+	return str;
+}
+
+//just in case we optimize...
+#define g_hash_table_insert_int64(a, b, c) g_hash_table_insert((a), &(b), (c))
+#define g_hash_table_replace_int64(a, b, c) g_hash_table_replace((a), &(b), (c))
+#define g_hash_table_steal_int64(a, b) g_hash_table_steal((a), &(b))
+#define g_hash_table_lookup_int64(a, b) g_hash_table_lookup((a), &(b))
+#define g_hash_table_contains_int64(a, b) g_hash_table_contains((a), &(b))
+#define g_hash_table_lookup_extended_int64(a, b, c, d) g_hash_table_lookup_extended((a), &(b), (c), (d))
