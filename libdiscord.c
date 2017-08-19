8f8d941a5e4dcb877ded1c9449c51abe33fb6af2
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-08-18 17:25:56 -0700

    Remove broken type field

--- libdiscord.c
+++ libdiscord.c
@@ -1817,11 +1817,8 @@ discord_got_group_dm(DiscordAccount *da, JsonObject *data)
 
 	GHashTable *components = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 
-	DiscordChannelType channel_type = CHANNEL_GROUP_DM;
-
 	g_hash_table_replace(components, g_strdup("id"), g_strdup_printf("%" G_GUINT64_FORMAT, channel->id));
 	g_hash_table_replace(components, g_strdup("name"), g_strdup(channel->name));
-	g_hash_table_replace(components, g_strdup("type"), g_memdup(&channel_type, sizeof(channel_type)));
 
 	PurpleGroup *group = discord_get_or_create_default_group();
 	PurpleChat *chat = purple_chat_new(da->account, channel->name, components);
