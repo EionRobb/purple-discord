d459db33524eb01e222f82876f249712bf21e70d
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-08-29 15:35:42 -0700

    *coughs*

--- libdiscord.c
+++ libdiscord.c
@@ -4544,7 +4544,7 @@ discord_got_info(DiscordAccount *da, JsonNode *node, gpointer user_data)
 
 	if (json_array_get_length(connected_accounts)) {
 		purple_notify_user_info_add_section_break(user_info);
-		purple_notify_user_info_add_pair_html(user_info, _("Connected Accunts"), user->game);
+		purple_notify_user_info_add_pair_html(user_info, _("Connected Accounts"), user->game);
 	}
 
 	for (i = json_array_get_length(connected_accounts) - 1; i >= 0; i--) {
