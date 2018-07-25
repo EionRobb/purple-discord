c17274cb160a8c2e30f7f60e95a7389391c4aec9
Vasily Alexandrovich Okunevs <silent1st@outlook.com>
2018-07-25 12:21:52 +0300

    Update autocompile.sh

--- autocompile.sh
+++ autocompile.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo apt-get install gettext
-sudo apt-get install git
+sudo apt-get install gettext git libglib2.0-dev libjson-glib-dev libpurple-dev
 git clone git://github.com/EionRobb/purple-discord.git
 cd purple-discord
 make
