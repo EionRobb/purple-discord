EionRobb/purple-discord

commit 48745ec038463393bd55fdffc60e6aacd1b1fbc6

Fix compiler errors from previous PR

markdown.c
@@ -250,7 +250,9 @@ markdown_convert_markdown(const gchar *html, gboolean escape_html, gboolean disc
 		} else if (c == '|') {
 			if (html[i + 1] == '|') {
 #ifdef MARKDOWN_PIDGIN
-					HTML_TOGGLE_OUT(s_spoiler, "<span style='foreground: black; background: black'>", "</span>");
+				HTML_TOGGLE_OUT(s_spoiler, "<span style='foreground: black; background: black'>", "</span>");
+#else
+				HTML_TOGGLE_OUT(s_codebit, "<details><summary>Spoiler</summary>", "</details>");
 #endif
 				i++;
 			}

Makefile
@@ -85,7 +85,7 @@ else
   endif
 endif
 
-WIN32_CFLAGS = -std=c99 -I$(WIN32_DEV_TOP)/glib-2.28.8/include -I$(WIN32_DEV_TOP)/glib-2.28.8/include/glib-2.0 -I$(WIN32_DEV_TOP)/glib-2.28.8/lib/glib-2.0/include -I$(WIN32_DEV_TOP)/json-glib-0.14/include/json-glib-1.0 -DENABLE_NLS -DDISCORD_PLUGIN_VERSION='"$(PLUGIN_VERSION)"' -Wall -Wextra -Werror -Wno-deprecated-declarations -Wno-unused-parameter -fno-strict-aliasing -Wformat
+WIN32_CFLAGS = -std=c99 -I$(WIN32_DEV_TOP)/glib-2.28.8/include -I$(WIN32_DEV_TOP)/glib-2.28.8/include/glib-2.0 -I$(WIN32_DEV_TOP)/glib-2.28.8/lib/glib-2.0/include -I$(WIN32_DEV_TOP)/json-glib-0.14/include/json-glib-1.0 -DENABLE_NLS -DDISCORD_PLUGIN_VERSION='"$(PLUGIN_VERSION)"' -DMARKDOWN_PIDGIN -Wall -Wextra -Werror -Wno-deprecated-declarations -Wno-unused-parameter -fno-strict-aliasing -Wformat
 WIN32_LDFLAGS = -L$(WIN32_DEV_TOP)/glib-2.28.8/lib -L$(WIN32_DEV_TOP)/json-glib-0.14/lib -lpurple -lintl -lglib-2.0 -lgobject-2.0 -ljson-glib-1.0 -g -ggdb -static-libgcc -lz
 WIN32_PIDGIN2_CFLAGS = -I$(PIDGIN_TREE_TOP)/libpurple -I$(PIDGIN_TREE_TOP) $(WIN32_CFLAGS)
 WIN32_PIDGIN3_CFLAGS = -I$(PIDGIN3_TREE_TOP)/libpurple -I$(PIDGIN3_TREE_TOP) -I$(WIN32_DEV_TOP)/gplugin-dev/gplugin $(WIN32_CFLAGS)

libdiscord.c
@@ -1783,14 +1783,14 @@ discord_download_image_cb(DiscordAccount *da, JsonNode *node, gpointer user_data
 			purple_serv_got_im(da->pc, img_context->from, attachment_show, img_context->flags, img_context->timestamp);
 		}
 		g_free(attachment_show);
-		discord_free_image_contex(img_context);
+		discord_free_image_context(img_context);
 		return;
 	} else {
 		purple_debug_error("discord", "Image response node is null!\n");
-		discord_free_image_contex(img_context);
+		discord_free_image_context(img_context);
 		return;
 	}
-	discord_free_image_contex(img_context);
+	discord_free_image_context(img_context);
 	return;
 }
 

commit 912fc756243303dbdc06c38d76ba93e13ba5db31

Use correct s_spoiler variable for spoiler HTML toggle
Fixes: commit 48745ec038463393bd55fdffc60e6aacd1b1fbc6
Suggested-by: GCC

markdown.c: In function ‘markdown_convert_markdown’:
markdown.c:144:11: warning: unused variable ‘s_spoiler’ [-Wunused-variable]
  144 |  gboolean s_spoiler = FALSE;
      |           ^~~~~~~~~

markdown.c
@@ -252,7 +252,7 @@ markdown_convert_markdown(const gchar *html, gboolean escape_html, gboolean disc
 #ifdef MARKDOWN_PIDGIN
 				HTML_TOGGLE_OUT(s_spoiler, "<span style='foreground: black; background: black'>", "</span>");
 #else
-				HTML_TOGGLE_OUT(s_codebit, "<details><summary>Spoiler</summary>", "</details>");
+				HTML_TOGGLE_OUT(s_spoiler, "<details><summary>Spoiler</summary>", "</details>");
 #endif
 				i++;
 			}