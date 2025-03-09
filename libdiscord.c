61f0a0aa66423c95e09417778e02f256e89a9e67
Boris Gjenero <boris.gjenero@gmail.com>
2025-03-09 16:06:40 -0400

    Rename email field to login to fix #468

--- libdiscord.c
+++ libdiscord.c
@@ -5773,9 +5773,9 @@ discord_login_response(DiscordAccount *da, JsonNode *node, gpointer user_data)
 			return;
 		}
 
-		if (json_object_has_member(response, "email")) {
+		if (json_object_has_member(response, "login")) {
 			/* Probably an error about new location */
-			purple_connection_error(da->pc, PURPLE_CONNECTION_ERROR_AUTHENTICATION_FAILED, json_object_get_string_member(response, "email"));
+			purple_connection_error(da->pc, PURPLE_CONNECTION_ERROR_AUTHENTICATION_FAILED, json_object_get_string_member(response, "login"));
 			return;
 		}
 
@@ -5794,8 +5794,8 @@ discord_login_response(DiscordAccount *da, JsonNode *node, gpointer user_data)
 		// {"message": "Invalid Form Body", "code": 50035, "errors": {"email": {"_errors": [{"code": "ACCOUNT_COMPROMISED_RESET_PASSWORD", "message": "Please reset your password to log in."}]}}}
 		if (json_object_has_member(response, "errors")) {
 			JsonObject *errors = json_object_get_object_member(response, "errors");
-			if (json_object_has_member(errors, "email")) {
-				JsonObject *email = json_object_get_object_member(errors, "email");
+			if (json_object_has_member(errors, "login")) {
+				JsonObject *email = json_object_get_object_member(errors, "login");
 				if (json_object_has_member(email, "_errors")) {
 					JsonArray *email_errors = json_object_get_array_member(email, "_errors");
 					JsonObject *email_error = json_array_get_object_element(email_errors, 0);
@@ -5879,7 +5879,7 @@ discord_login(PurpleAccount *account)
 		JsonObject *data = json_object_new();
 		gchar *str;
 
-		json_object_set_string_member(data, "email", purple_account_get_username(account));
+		json_object_set_string_member(data, "login", purple_account_get_username(account));
 		json_object_set_string_member(data, "password", account_password);
 
 		str = json_object_to_string(data);
